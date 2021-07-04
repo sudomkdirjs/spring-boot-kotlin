7a34dc2eae95be07bbfb1b0b102a09b14bcfe02a
marees <mareeswaran.sooriamoorthi@hidglobal.com>
2021-07-04 22:03:02 +0530

    Added HelloWorldController

---
+++ src/main/kotlin/com/sudomkdirjs/marees/controller/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package com.sudomkdirjs.marees.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWorld(): String = "Hello, This is REST endpoint!"
+}
