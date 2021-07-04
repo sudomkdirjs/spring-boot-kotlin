f0522dc7f7565f92c8cda6a7104f52a27ed63890
marees <mareeswaran.sooriamoorthi@hidglobal.com>
2021-07-04 22:48:47 +0530

    Added Bank data class

---
+++ src/main/kotlin/com/sudomkdirjs/marees/model/Bank.kt
@@ -0,0 +1,11 @@
+package com.sudomkdirjs.marees.model
+
+/**
+ * Primary Constructor user here.
+ * By default data class will have default implementation of equals, hashCode and toString methods
+ */
+data class Bank(
+    val accountNumber: String,
+    val trust: Double,
+    val transactionFee: Int) {
+}
