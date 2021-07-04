d37fe8289ca0cea4d512738a77c05b8a64e2cb6b
marees <mareeswaran.sooriamoorthi@hidglobal.com>
2021-07-04 20:53:10 +0530

    Initial commit

---
+++ src/main/kotlin/com/sudomkdirjs/marees/MareesApplication.kt
@@ -0,0 +1,11 @@
+package com.sudomkdirjs.marees
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MareesApplication
+
+fun main(args: Array<String>) {
+	runApplication<MareesApplication>(*args)
+}
---
+++ src/test/kotlin/com/sudomkdirjs/marees/MareesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sudomkdirjs.marees
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MareesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
